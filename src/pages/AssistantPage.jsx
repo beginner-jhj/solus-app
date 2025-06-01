e756d97b221369bec445130562d23439db0d9a2e
jhj <hj5014221@gmail.com>
2025-06-01 17:09:51 +0900

    renamed  img src

--- src/pages/AssistantPage.jsx
+++ src/pages/AssistantPage.jsx
@@ -1,10 +1,10 @@
-import talkIcon from "../assets/talk-icon.svg";
+import chatHistoryIcon from "../assets/talk-icon.svg";
 import {ConversationContainer} from "../components/assistant/ConversationContainer";
 
 export default function AssistantPage() {
   return (
     <div className="w-full h-full flex flex-col items-start justify-start">
-      <img src={talkIcon} className="w-4 h-4 cursor-pointer" />
+      <img src={chatHistoryIcon} className="w-4 h-4 cursor-pointer" />
       <ConversationContainer />
     </div>
   );
